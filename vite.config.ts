c8b3b45bad11988568b4572c7a783ead68e421b9
lupusmartius <derbrander@gmail.com>
2026-03-14 21:55:10 +0200

    fix base for githun pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react_phone-catalog/',
+  base: '/react.phone.catalog/',
   resolve: {
     dedupe: [
       'react',
